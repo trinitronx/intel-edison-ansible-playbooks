b8c4b7e3fe6e90fb4afc93d072b8542b2e56512a
James Cuzella <james.cuzella@lyraphase.com>
2022-01-10 03:36:42 -0700

    Setup docker-machine VM shared folder mount + user & group
    Change-Id: I8aefa541bab63a6be8a08606061ddb683c7c82d5

--- bin/docker_machine_create_dm.sh
+++ bin/docker_machine_create_dm.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+docker_machine_state() {
+  docker_machine_name="$1"
+  docker-machine ls --filter "Name=$docker_machine_name" --format '{{.State}}'
+}
+
+errorout() {
+  echo -e "\x1b[31;1mERROR:\x1b[0m ${1}"; exit 1
+}
+
 # Notes:
 #  - Both docker-machine and boot2docker
 #    are marked as Deprecated by the original authors
@@ -21,3 +30,23 @@ docker-machine create --driver virtualbox \
   --virtualbox-boot2docker-url "$BOOT2DOCKER_URL" \
   --virtualbox-hostonly-cidr "192.168.56.100/21" \
   dm
+
+eval $(docker-machine env dm)
+
+DOCKER_MACHINE_NAME=$(docker-machine active 2>/dev/null)
+[ -z $DOCKER_MACHINE_NAME ] && DOCKER_MACHINE_NAME=$(docker-machine active | head -n1)
+[ -z $DOCKER_MACHINE_NAME ] && errorout "Cannot find active docker-machine name... make sure the VM is running"
+
+echo "Creating vboxsf group INSIDE docker-machine VM: $DOCKER_MACHINE_NAME"
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo addgroup -g 20 vboxsf"
+echo "Adding docker user to vboxsf group"
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo addgroup docker vboxsf"
+
+echo "Starting VBoxService automount /Users shared folder"
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo VBoxService --only-automount"
+# TODO: Fix "magic"? automount path + uid / gid setup
+# Reference:
+#  -  https://github.com/docker/machine/blob/b170508bf44c3405e079e26d5fdffe35a64c6972/drivers/virtualbox/virtualbox.go#L445
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo umount /sf_Users"
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo mkdir /Users; sudo mount -t vboxsf -o uid=$(id -u),gid=$(id -g) Users /Users"
+docker-machine ssh $DOCKER_MACHINE_NAME "sudo mount -t vboxsf -o uid=$(id -u),gid=$(id -g) Users /Users"
