1c44b367c4c2f0c3e7e561af8f9dadc054e6699c
James Cuzella <james.cuzella@lyraphase.com>
2022-01-09 19:12:21 -0700

    Fix docker-machine install python script
    Change-Id: Ia5ddbba4a78d87887d614f731be73042e0bd2855

--- bin/docker_machine_install_python.sh
+++ bin/docker_machine_install_python.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-#http://stackoverflow.com/questions/30605742/issues-getting-ansible-to-work-with-boot2docker
+# http://stackoverflow.com/questions/30605742/issues-getting-ansible-to-work-with-boot2docker
+
+docker_machine_state() {
+  docker_machine_name="$1"
+  docker-machine ls --filter "Name=$docker_machine_name" --format '{{.State}}'
+}
+
+DOCKER_MACHINE_NAME=$(docker-machine active 2>/dev/null)
+[ -z $DOCKER_MACHINE_NAME ] && DOCKER_MACHINE_NAME=$(docker-machine ls --filter 'driver=virtualbox' --format '{{.Name}}' | head -n1)
+
+[[ "$(docker-machine status $DOCKER_MACHINE_NAME)" != 'Running' ]] && docker-machine start $DOCKER_MACHINE_NAME
+
 # To use Ansible in docker-machine, we must install python in docker-machine VM (it's based on Tiny Core Linux)
-docker-machine ssh "wget http://www.tinycorelinux.net/6.x/x86/tcz/python.tcz && tce-load -i python.tcz && rm -f python.tcz"
+echo "Installing qemu-arm-static to /usr/bin/qemu-arm-static INSIDE docker-machine VM: $DOCKER_MACHINE_NAME"
+docker-machine ssh $DOCKER_MACHINE_NAME "wget http://www.tinycorelinux.net/6.x/x86/tcz/python.tcz && tce-load -i python.tcz && rm -f python.tcz"
