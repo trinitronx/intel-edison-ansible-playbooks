a69e2e2b10b90c3e292ce99b452dbbcb8f7258a2
James Cuzella <james.cuzella@lyraphase.com>
2022-02-16 01:37:54 -0700

    Fix all shellcheck issues with: bin/docker_machine_set_ntp_time.sh
    Change-Id: I2d4f804003d1b84929ca784b12a1c35f8d5ea500

--- bin/docker_machine_set_ntp_time.sh
+++ bin/docker_machine_set_ntp_time.sh
@@ -7,9 +7,9 @@ docker_machine_state() {
 
 
 DOCKER_MACHINE_NAME=$(docker-machine active 2>/dev/null)
-[ -z $DOCKER_MACHINE_NAME ] && DOCKER_MACHINE_NAME=$(docker-machine ls --filter 'driver=virtualbox' --format '{{.Name}}' | head -n1)
+[ -z "$DOCKER_MACHINE_NAME" ] && DOCKER_MACHINE_NAME=$(docker-machine ls --filter 'driver=virtualbox' --format '{{.Name}}' | head -n1)
 
-[[ "$(docker-machine status $DOCKER_MACHINE_NAME)" != 'Running' ]] && docker-machine start $DOCKER_MACHINE_NAME
+[[ "$(docker-machine status "$DOCKER_MACHINE_NAME")" != 'Running' ]] && docker-machine start "$DOCKER_MACHINE_NAME"
 
-docker-machine ssh $DOCKER_MACHINE_NAME  'tce-load -wi ntpclient.tcz'
-docker-machine ssh $DOCKER_MACHINE_NAME  'ntpclient -c 10 -i 1 -q 300 -l -d -h 0.amazon.pool.ntp.org'
+docker-machine ssh "$DOCKER_MACHINE_NAME"  'tce-load -wi ntpclient.tcz'
+docker-machine ssh "$DOCKER_MACHINE_NAME"  'ntpclient -c 10 -i 1 -q 300 -l -d -h 0.amazon.pool.ntp.org'
