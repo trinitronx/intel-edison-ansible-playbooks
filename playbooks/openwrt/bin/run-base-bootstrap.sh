356f4f3cb5553c329a9a3e293443811ee50ba5fc
James Cuzella <james.cuzella@lyraphase.com>
2022-02-16 02:27:15 -0700

    Fix shellcheck issues with: playbooks/openwrt/bin/run-base-bootstrap.sh
    Change-Id: Ief5ad6e8bf375d8142e89b1940a900106d8a5cc8

--- playbooks/openwrt/bin/run-base-bootstrap.sh
+++ playbooks/openwrt/bin/run-base-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT=$(basename $0)
+SCRIPT=$(basename "$0")
 REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 # If using docker-machine + VirtualBox...
@@ -9,8 +9,8 @@ REPO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 DOCKER_MACHINE_NAME=$(docker-machine active 2>/dev/null)
 
 if [ -n "${DOCKER_MACHINE_NAME}" ]; then
-  VBoxManage modifyvm $DOCKER_MACHINE_NAME --natdnshostresolver1 on
+  VBoxManage modifyvm "$DOCKER_MACHINE_NAME" --natdnshostresolver1 on
 fi
 
 # First run must SSH in as root with asked password (we assume you have set a password first)
-ansible-playbook -i ${REPO_BASE}/inventory/hosts ${REPO_BASE}/base.yml -vv --diff --user=root --ask-pass
+ansible-playbook -i "${REPO_BASE}"/inventory/hosts "${REPO_BASE}"/base.yml -vv --diff --user=root --ask-pass
